919bff1d200648ea9c3c96b3273757e21c6f17d3
Andreas Schempp <andreas.schempp@terminal42.ch>
2013-08-15 10:39:47 +0200

    Added german translation

---
+++ languages/de/tl_form_mails.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ * Copyright (C) 2005-2012 Leo Feyer
+ *
+ * Formerly known as TYPOlight Open Source CMS.
+ *
+ * This program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program. If not, please visit the Free
+ * Software Foundation website at <http://www.gnu.org/licenses/>.
+ *
+ * PHP version 5
+ * @copyright  terminal42 gmbh 2010-2012
+ * @author     Andreas Schempp <andreas.schempp@terminal42.ch>
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+ */
+
+
+/**
+ * Fields
+ */
+$GLOBALS['TL_LANG']['tl_form_mails']['tstamp']			= array('Versendet');
+$GLOBALS['TL_LANG']['tl_form_mails']['cmailRecipient']	= array('Empfänger');
+$GLOBALS['TL_LANG']['tl_form_mails']['cmailBcc']		= array('Zusätzliche Empfänger');
+$GLOBALS['TL_LANG']['tl_form_mails']['cmailSender']		= array('Absender');
+$GLOBALS['TL_LANG']['tl_form_mails']['cmailSubject']	= array('Betreff');
+$GLOBALS['TL_LANG']['tl_form_mails']['cmailMessage']	= array('Nachricht');
+$GLOBALS['TL_LANG']['tl_form_mails']['source']			= array('Quelldatei', 'Wählen Sie eine oder mehrere Dateien für den CSV-Import');
+
+
+/**
+ * Buttons
+ */
+$GLOBALS['TL_LANG']['tl_form_mails']['delete']		= array('E-Mail löschen', 'E-Mail ID %s löschen');
+$GLOBALS['TL_LANG']['tl_form_mails']['show']		= array('E-Mail Details', 'Details der E-Mail ID %s anzeigen');
+$GLOBALS['TL_LANG']['tl_form_mails']['import']		= array('CSV importieren', 'CSV-Dateien aus AutoForm importieren');
+$GLOBALS['TL_LANG']['tl_form_mails']['export']		= array('CSV exportieren', 'In CSV-Datei exportieren');
+
+
+/**
+ * References
+ */
+$GLOBALS['TL_LANG']['tl_form']['id']				= array('Formular-ID');
